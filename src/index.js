8de588b0479bd1c6654012ce39e11928022fd6ba
atiTaj <atietajnorouzi@gmail.com>
2023-04-16 17:25:48 +0430

    changed to final shecode plus project

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ function changeCelsiusTemp(event){
   temp.innerHTML= celsiusTemp;
 }
 
+function setFirstWeatherIcon(){
+let nowTimestamp = new Date();
+let hours = nowTimestamp.getHours();
+let firstIcon = document.querySelector("#icon")
+if( hours > 5 && hours < 17){
+  firstIcon.setAttribute("src","images/sun.svg");
+}
+
+}
 function changeFarenheitTemp(event){
   event.preventDefault();
   celsius.classList.remove("active");
@@ -40,10 +49,47 @@ let day = date.getDay();
 return`${days[day]} ${hours}:${minutes}`;
 }
 
+
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+return days[day];
+}
+
+function displayForecast(response){
+  let forecastResponse = response.data.daily;
+  let forecast = document.querySelector('#forecast');
+  let forecastHtml = `<div class="row">`;
+
+  forecastResponse.forEach(function (forecastDay , index ){
+    if(index < 6){
+       forecastHtml = forecastHtml +  `
+              <div class="col-2">
+                <div class="forcast-date">${formatDay(forecastDay.dt)}</div>
+                <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" id="forcast-icon"/>
+                <div class="forcast-temp">
+                  <span class="temp-max">${Math.round(forecastDay.temp.max)}°</span>
+                  <span class="temp-min">${Math.round(forecastDay.temp.min)}°</span>
+                </div>
+              </div>
+            `;
+    }
+
+  })
+
+  forecastHtml = forecastHtml +`</div>`;
+  forecast.innerHTML = forecastHtml;
+}
+
+function getForecast(coordinates){  
+// forecast weather api
+  let apiKey = "15b6ba0523386a8a73b38b2440a74dea";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
  function logResponse(response) {
- debugger;
   let cityInfo = document.querySelector("#search");
-  console.log(response.data)
   cityInfo.innerHTML = `${response.data.name} <br/> ${formatDate(response.data.dt * 1000)} <br/> ${response.data.weather[0].description} <br/>`;
 
   let temperture = document.querySelector("#temp");
@@ -57,31 +103,9 @@ return`${days[day]} ${hours}:${minutes}`;
   let icon =  document.querySelector("#icon");
   icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) ;
   icon.setAttribute("alt" , response.data.weather[0].description);
- 
+  getForecast(response.data.coord);
 }
 
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20
-  }
-};
 
 let days = [
   "Sunday",
@@ -92,20 +116,25 @@ let days = [
   "Friday",
   "Saturday"
 ];
-
+debugger
+setFirstWeatherIcon();
 let button = document.querySelector("button");
 button.addEventListener("click", search);
 let celsiusTemp = null;
 let celsius = document.querySelector("#celsius");
-celsius.addEventListener("click",changeCelsiusTemp )
+celsius.addEventListener("click", changeCelsiusTemp);
+
+window.addEventListener("load",  getCurrent )
+
 
 let farenheit = document.querySelector("#farenheit");
-farenheit.addEventListener("click",changeFarenheitTemp )
+farenheit.addEventListener("click", changeFarenheitTemp)
 
 let current = document.querySelector("#current");
-current.addEventListener("click",getCurrent )
+current.addEventListener("click", getCurrent)
  
 let city = document.querySelector("#city-input");
 
+// weather api
 let apiKey = "22a0bff1a7903c62ba5b8be15b070e0f";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric`;
